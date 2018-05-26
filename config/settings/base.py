6549c3c9f090deb07d21a82b3527fb0833d2390b
tonky0110 <tonky0110@gmail.com>
2018-05-27 08:30:18 +0900

    Nomadgram 1-19
    images app 추가 및 테이블 컬럼 추가 후 동기화

--- config/settings/base.py
+++ config/settings/base.py
@@ -72,6 +72,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'nomadgram2.users.apps.UsersConfig',
     # Your stuff: custom apps go here
+    'nomadgram2.images.apps.ImagesConfig',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
